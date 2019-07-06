b4351d3122f1270f96ab158456a6d3286c202e64
liupenghao <haochen214@163.com>
2019-07-06 15:26:27 +0800

    添加项目文件。

---
+++ SingletonSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingletonSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
